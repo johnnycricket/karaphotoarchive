cacaa2010f4c8b980480882fda714a9f7a404e6e
John Vorwald <cricketjohnny@gmail.com>
2021-04-30 16:03:33 -0500

    first commit. run all the basics for amplify.:

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
+
+const App = () => {
+  return (
+    <div className="App">
+      <nav className="app-nav"><AmplifySignOut /></nav>
+
+        Kara Photo Storage
+      </header>
+      <div className="content">
+        Hi
+      </div>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
